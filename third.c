a36d1030e41e26989ff85c741a68cec10a2bef69
mahteeh <33111294+mahteeh@users.noreply.github.com>
2018-04-04 23:42:19 +0530

    Add files via upload

---
+++ third.c
@@ -0,0 +1,38 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/types.h>
+int main()
+{
+	int n,i;
+	printf("enter number of processes");
+	scanf("%d",&n);
+	int processes[n];
+	int burst_time[n];
+	int waiting_time[n];
+	int arrival_time[n];
+	int priority[n];
+	
+	printf("enter the burst time for each process \n");
+	for(i=0;i<n;i++)
+	{
+		printf("process %d ",i+1);
+		scanf("%d",&burst_time[i]);
+	}
+	printf("enter the arrival time for each process \n");
+	for(i=0;i<n;i++)
+	{
+		printf("process %d ",i+1);
+		scanf("%d",&arrival_time[i]);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		priority[i]=(1+waiting_time[i])/burst_time[i];
+	}
+	
+	printf("process busrt_time arrival_time");
+	for(i=0;i<n;i++)
+	{
+		printf("\n %3d  %10d %10d",i+1,burst_time[i],waiting_time[i]);
+	}
+}
